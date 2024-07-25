b8e955669b2753d28708422e8128a1518808468d
Mohit2005123 <mohitmongia2005@gmail.com>
2024-07-25 12:31:09 +0530

    Fixed resizing issue for features component

--- Frontend/src/components/Features.jsx
+++ Frontend/src/components/Features.jsx
@@ -1,120 +1,124 @@
 import React from "react";
-import {Card, CardHeader, CardBody, CardFooter, Image, Button} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
 
-function Features(){
-    return (
-        <div>
-             <section className="py-12">
-                    <div className="container mx-auto">
-                        <div className="flex flex-wrap">
-                            <div className="lg:w-1/3 md:w-1/2 p-4 w-full">
-                                <div className="content-center">
-                                    <span className="text-yellow-500">Why choose us?</span>
-                                    <h2 className="text-4xl font-bold">Our feature</h2>
-                                    <p className="my-6">Step 1: Users registers and fills out the necessary information. <br />
-                                        Step 2: Select the doctor as per your requirement. <br />
-                                        Step 3: Select the doctor and book an appointment as per the availability. You may also mention brief information about your problems. <br />
-                                        Step 4: Once the appointment is confirmed you may view it in the schedule section. <br />
-                                        Step 5: Before users actually visit the doctor, they have the option to easily upload and share health-related information and documents online. <br />
-                                    </p>
-                                    <a href="#" className="btn bg-[#0C2B4B] text-white py-2 px-6">See Doctors</a>
-                                </div>
-                            </div>
-                            <CardStructure></CardStructure>
-                        </div>
-                    </div>
-                </section>
-
-        </div>
-    )
-}
-const CardStructure= ()=>{
-    return(
-        <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
-        <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-4.jpeg"
-      />
-    </Card>
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
-        <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-3.jpeg"
-      />
-    </Card>
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
-        <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-2.jpeg"
-      />
-    </Card>
-    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-        <h4 className="text-black font-medium text-2xl">Acme camera</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card example background"
-        className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-        src="https://nextui.org/images/card-example-6.jpeg"
-      />
-      <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-        <div>
-          <p className="text-black text-tiny">Available soon.</p>
-          <p className="text-black text-tiny">Get notified.</p>
-        </div>
-        <Button className="text-tiny" color="primary" radius="full" size="sm">
-          Notify Me
-        </Button>
-      </CardFooter>
-    </Card>
-    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
-        <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Relaxing app background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-5.jpeg"
-      />
-      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-        <div className="flex flex-grow gap-2 items-center">
-          <Image
-            alt="Breathing app icon"
-            className="rounded-full w-10 h-11 bg-black"
-            src="https://nextui.org/images/breathing-app-icon.jpeg"
-          />
-          <div className="flex flex-col">
-            <p className="text-tiny text-white/60">Breathing App</p>
-            <p className="text-tiny text-white/60">Get a good night's sleep.</p>
+function Features() {
+  return (
+    <div>
+      <section className="py-12">
+        <div className="container mx-auto">
+          <div className="flex flex-wrap">
+            <div className="lg:w-1/3 md:w-1/2 p-4 w-full">
+              <div className="content-center">
+                <span className="text-yellow-500">Why choose us?</span>
+                <h2 className="text-4xl font-bold">Our feature</h2>
+                <p className="my-6">
+                  Step 1: Users register and fill out the necessary information. <br />
+                  Step 2: Select the doctor as per your requirement. <br />
+                  Step 3: Select the doctor and book an appointment as per the availability. You may also mention brief information about your problems. <br />
+                  Step 4: Once the appointment is confirmed, you may view it in the schedule section. <br />
+                  Step 5: Before users actually visit the doctor, they have the option to easily upload and share health-related information and documents online. <br />
+                </p>
+                <a href="#" className="btn bg-[#0C2B4B] text-white py-2 px-6">See Doctors</a>
+              </div>
+            </div>
+            <div className="lg:w-2/3 md:w-1/2 p-4 w-full">
+              <CardStructure />
+            </div>
           </div>
         </div>
-        <Button radius="full" size="sm">Get App</Button>
-      </CardFooter>
-    </Card>
-  </div>
-    )
+      </section>
+    </div>
+  );
 }
+
+const CardStructure = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Card className="h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
+          <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card background"
+          className="z-0 w-full h-full object-cover"
+          src="https://nextui.org/images/card-example-4.jpeg"
+        />
+      </Card>
+      <Card className="h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
+          <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card background"
+          className="z-0 w-full h-full object-cover"
+          src="https://nextui.org/images/card-example-3.jpeg"
+        />
+      </Card>
+      <Card className="h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
+          <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card background"
+          className="z-0 w-full h-full object-cover"
+          src="https://nextui.org/images/card-example-2.jpeg"
+        />
+      </Card>
+      <Card isFooterBlurred className="w-full h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">New</p>
+          <h4 className="text-black font-medium text-2xl">Acme camera</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card example background"
+          className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+          src="https://nextui.org/images/card-example-6.jpeg"
+        />
+        <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+          <div>
+            <p className="text-black text-tiny">Available soon.</p>
+            <p className="text-black text-tiny">Get notified.</p>
+          </div>
+          <Button className="text-tiny" color="primary" radius="full" size="sm">
+            Notify Me
+          </Button>
+        </CardFooter>
+      </Card>
+      <Card isFooterBlurred className="w-full h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
+          <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Relaxing app background"
+          className="z-0 w-full h-full object-cover"
+          src="https://nextui.org/images/card-example-5.jpeg"
+        />
+        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+          <div className="flex flex-grow gap-2 items-center">
+            <Image
+              alt="Breathing app icon"
+              className="rounded-full w-10 h-11 bg-black"
+              src="https://nextui.org/images/breathing-app-icon.jpeg"
+            />
+            <div className="flex flex-col">
+              <p className="text-tiny text-white/60">Breathing App</p>
+              <p className="text-tiny text-white/60">Get a good night's sleep.</p>
+            </div>
+          </div>
+          <Button radius="full" size="sm">Get App</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
 export default Features;
